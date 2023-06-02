4c937b2a8766b55d5b18ff055ab86b3ecd7917dc
AdirthaBorgohain <adirthaborgohain@gmail.com>
2023-06-02 11:53:07 +0530

    fix for cases when using transformers

--- PipelineModel.py
+++ PipelineModel.py
@@ -16,14 +16,14 @@ class DotDict(dict):
 
 class PipelineModel:
     def __init__(self) -> None:
-        self.__pipeline_config = self.load_yaml_file('./pipeline.yml')
-        self.__model = spacy.load(self.__pipeline_config.vars['ner_model'])
-        self.__model = attach_linker(spacy_model=self.__model)
-        self.__re = spacy.load(self.__pipeline_config.vars['re_model'])
-        self.__model.add_pipe(
-            "tok2vec", name="re_tok2vec", source=self.__re)
-        self.__model.add_pipe("relation_extractor",
-                       source=self.__re, last=True)
+        self._pipeline_config = self.load_yaml_file('./pipeline.yml')
+        self._model = spacy.load(self._pipeline_config.vars['ner_model'])
+        self._model = attach_linker(spacy_model=self._model)
+        self._re = spacy.load(self._pipeline_config.vars['re_model'])
+        self._model.add_pipe(
+            self._re.component_names[0], name=f"re_{self._re.component_names[0]}", source=self._re)
+        self._model.add_pipe("relation_extractor",
+                             source=self._re, last=True)
 
     @staticmethod
     def load_yaml_file(file_name):
@@ -32,10 +32,10 @@ class PipelineModel:
         return DotDict(doc_dict)
 
     def get_predictions(self, text: str, threshold: float = 0.4):
-        doc = self.__model(text)
+        doc = self._model(text)
         print(f"Text: {text}\n")
         print(f"Extracted Entities -> {[(e.text, e.label_) for e in doc.ents]}\n")
-        linker = self.__model.get_pipe("scispacy_linker")
+        linker = self._model.get_pipe("scispacy_linker")
         print(f"Linked Entities in Knowledge Base ->")
         for entity in doc.ents:
             for ent in entity._.kb_ents:
@@ -57,5 +57,5 @@ class PipelineModel:
 if __name__ == "__main__":
     pipeline = PipelineModel()
     sample_text = "The up-regulation of RNA was characteristic of an early inducible gene, with " \
-        "maximal upregulation two hours after the addition of BMP-6 and returned to baseline after 24 hours."
+                  "maximal upregulation two hours after the addition of BMP-6 and returned to baseline after 24 hours."
     pipeline.get_predictions(text=sample_text)
